7f2b9aa93671902900abdb193791f4369d31ac69
Lucas Sertelon <lucas.sertelon@gmail.com>
2025-01-25 18:37:30 +0100

    Version : 2.0 Jouable // Ajoute du systeme d'équipement avec augmentation et reduction de dégat + amélioration du systeme de combat

--- Main.py
+++ Main.py
@@ -24,6 +24,7 @@ Inventory = {
 
 Player_stat = {
     "PV" : 100,
+    "Base_domage" : 10,
     "Multiplicateur de dégat" : 10,
     "Inventaire" : Inventory,
     "Reduction de dégat" : 0,
@@ -52,9 +53,9 @@ Monsters = {  #### Value matching : (Difficuly, min dommage, max dommage, health
     "Gardien de Cristal" : ["Extreme", 30, 40, 60],
     "Démon Arcanique": ["Extreme", 40, 55, 70]}
 
-Armuor = { #### Value matching : (Protection, Drop rate)
-    "Armure Commune" : [0.10, 0.25],
-    "Armure Rare" : [0.25, 0.20],
+Armor = { #### Value matching : (Protection, Drop rate)
+    "Armure Commune" : [0.20, 0.25],
+    "Armure Rare" : [0.30, 0.20],
     "Armure Epic" : [0.50, 0.15],
     "Armure Legendaire" : [0.70, 0.10],
     "Bouclier Runique" : [0.30, 0.20],
@@ -166,6 +167,7 @@ def Display_inventory ():
     Dis_inv = []
     Dis_equip = Inventory["Equipement"]
     Dis_item = Inventory["Items"]
+    print ("Pour équiper une armure ou une arme saisiser le 'N°' de l'objet")
     for elem in Inventory :
         Dis_inv.append(elem)
     print ("Equipement équiper :")
@@ -187,6 +189,29 @@ def Display_inventory ():
     else :
         for i in range (len(Dis_item)):
             print (f"{i+1} - {Dis_item[i]}")
+ 
+    choix = input("-->")
+    if choix != "":
+        try:
+            verif = False
+            for elem in Armor :
+                if Dis_item[int(choix)-1] == elem : 
+                    Inventory["Equipement"][1] = Dis_item[int(choix)-1]
+                    Player_stat["Reduction de dégat"] = Armor[Dis_item[int(choix)-1]][0]
+                    verif = True
+                    print ("Equiper !!")
+            for elem in Weapons :
+                if Dis_item[int(choix)-1]== elem:
+                    Inventory["Equipement"][0] = Dis_item[int(choix)-1]
+                    Player_stat["Multiplicateur de dégat"] = Player_stat["Base_domage"] + Weapons[Dis_item[int(choix)-1]][0]
+                    verif = True
+                    print ("Equiper !!")
+            if verif == False : print("Vous ne pouvez pas équiper cette item")
+            time.sleep(3)
+        except:
+            print ("Saisie invalide")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    Display_map (map_forest) ## Refresh map
 
 def map_generation (x, y): ## Random map generation
     map_tmp = []
@@ -221,38 +246,36 @@ def interaction (posA) :
                             load_pv = Monsters[elem][3]
                             load_domage = randint(Monsters[elem][1], Monsters[elem][2])
                             print (elem,"vous attaque!!\n\n",elem,":\n________________________\n  HP :", load_pv,"   Dégat :", load_domage,"\n\n\nVous :\n________________________\n HP :", Player_stat["PV"], "   Dégat de base :", Player_stat["Multiplicateur de dégat"])
-                            if Inventory["Equipement"][0] == None :
-                                print ("\nAppuyer sur 'E' le plus de fois possible pour faire plus de dégat. (les attaques s'enchaine, il faut être réactif)")
-                                start = input("Ready? (Y/N)\n-->")
-                                start_loop = False 
-                                while start_loop == False :
-                                    if start.upper() == "Y" :
-                                        while load_pv > 0 and Player_stat["PV"] > 0:
-                                            os.system('cls' if os.name == 'nt' else 'clear')
-                                            print ("\n\n",elem,":\n________________________\n  HP :", load_pv,"   Dégat :", load_domage,"\n\n\nVous :\n________________________\n HP :", Player_stat["PV"], "   Dégat de base :", Player_stat["Multiplicateur de dégat"])
-                                            t = threading.Timer(5, lambda: print("\nAppuier sur 'enter' pour valider"))
-                                            t.start()
-                                            attack = input("Attaquer!!\n-->")
-                                            t.cancel()
-                                            print ("Dégat donner :", (len(attack)/20)*Player_stat["Multiplicateur de dégat"],"Dégat reçu :", load_domage)
-                                            load_pv -= (len(attack)/20)*Player_stat["Multiplicateur de dégat"]
-                                            Player_stat["PV"] -= load_domage
-                                            time.sleep(5)
+                            print ("\nAppuyer sur 'E' le plus de fois possible pour faire plus de dégat. (les attaques s'enchaine, il faut être réactif)")
+                            start = input("Ready? (Y/N)\n-->")
+                            start_loop = False 
+                            while start_loop == False :
+                                if start.upper() == "Y" :
+                                    while load_pv > 0 and Player_stat["PV"] > 0:
+                                        os.system('cls' if os.name == 'nt' else 'clear')
+                                        print ("\n\n",elem,":\n________________________\n  HP :", load_pv,"   Dégat :", load_domage,"\n\n\nVous :\n________________________\n HP :", Player_stat["PV"], "   Dégat de base :", Player_stat["Multiplicateur de dégat"])
+                                        t = threading.Timer(5, lambda: print("\nAppuier sur 'enter' pour valider"))
+                                        t.start()
+                                        attack = input("Attaquer!!\n-->")
+                                        t.cancel()
+                                        print ("Dégat donner :", (len(attack)/20)*Player_stat["Multiplicateur de dégat"],"Dégat reçu :", load_domage - (load_domage * Player_stat["Reduction de dégat"]))
+                                        load_pv -= (len(attack)/20)*Player_stat["Multiplicateur de dégat"]
+                                        Player_stat["PV"] -= load_domage - (load_domage * Player_stat["Reduction de dégat"])
+                                        time.sleep(5)
 
-                                        if Player_stat["PV"] == 0 :
-                                            start_loop = True
-                                            return False
-                                        else :
-                                            print ("Monstre vaincu !!\nBien jouer\n\n",elem,":\n________________________\n  HP :", 0,"   Dégat :", load_domage,"\n\n\nVous :\n________________________\n HP :", Player_stat["PV"], "   Dégat de base :", Player_stat["Multiplicateur de dégat"])
-                                            time.sleep(5)
-                                            posA = "'"
-                                            start_loop = True
-                                            return (True, posA)
-
-                                    elif start.upper() == "N" :
-                                        start_loop = (True, posA)
+                                    if Player_stat["PV"] == 0 :
+                                        start_loop = True
+                                        return False
                                     else :
-                                        print ("erreur de saisie")
+                                        print ("Monstre vaincu !!\nBien jouer\n\n",elem,":\n________________________\n  HP :", 0,"   Dégat :", load_domage,"\n\n\nVous :\n________________________\n HP :", Player_stat["PV"], "   Dégat de base :", Player_stat["Multiplicateur de dégat"])
+                                        time.sleep(5)
+                                        posA = "'"
+                                        return (True, posA)
+
+                                elif start.upper() == "N" :
+                                    start_loop = (True, posA)
+                                else :
+                                    print ("erreur de saisie")
                 count += 1 
     return (None, None)
 
